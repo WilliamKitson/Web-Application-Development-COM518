d329d5e711ee52c6c940ff5322a0b0220a12c155
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-04 11:39:16 +0000

    index.mjs redefined

--- index.mjs
+++ index.mjs
@@ -4,12 +4,49 @@ import Database from 'better-sqlite3';
 const app = express();
 const database = new Database('pointsofinterest.db');
 
+app.use(express.static('public'));
+app.use(express.json());
+
 app.get('/pointsofinterest/:region', (req, res) => {
     try {
         const stmt = database.prepare("SELECT * FROM pointsofinterest WHERE region=?");
         const results = stmt.all(req.params.region);
         res.json(results);
     } catch(error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+app.post('/pointsofinterest/create', (req, res) => {
+    const {
+        name,
+        type,
+        country,
+        region,
+        lat,
+        lon,
+        description,
+        recommendations
+    } = req.body;
+
+    try {
+        const stmt = database.prepare(
+            "INSERT INTO pointsofinterest(name, type, country, region, lat, lon, description, recommendations) " +
+            "VALUES(?,?,?,?,?,?,?,?)"
+        );
+
+        res.json(stmt.run(
+            name,
+            type,
+            country,
+            region,
+            lat,
+            lon,
+            description,
+            recommendations
+        ));
+    } catch(error) {
+        console.log(error);
         res.status(500).json({ error: error });
     }
 });
