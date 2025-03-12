3502893f860bbfb93d695a818930531de7daca48
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-12 16:14:31 +0000

    pointsOfInterest.jsx defined

---
+++ src/components/pointsOfInterest.jsx
@@ -0,0 +1,44 @@
+import React, {Fragment} from "react";
+
+function PointsOfInterest() {
+    return (
+        <Fragment>
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            Name
+                        </td>
+                        <td>
+                            Latitude
+                        </td>
+                        <td>
+                            Longitude
+                        </td>
+                        <td>
+                            Description
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            Name
+                        </td>
+                        <td>
+                            Latitude
+                        </td>
+                        <td>
+                            Longitude
+                        </td>
+                        <td>
+                            Description
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+export default PointsOfInterest;
