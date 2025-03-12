dfc9f09369aa6827787f0200b30d69aab36f6df4
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-12 15:33:07 +0000

    searchRegion.jsx redefined

--- src/components/searchRegion.jsx
+++ src/components/searchRegion.jsx
@@ -3,19 +3,36 @@ import React, {Fragment} from "react";
 function SearchRegion() {
     return (
         <Fragment>
-            <div>
-                <label>
-                    Region
-                    <input id={"region"} type={"text"} />
-                </label>
-                <button onClick={searchRegion}>
-                    Search
-                </button>
-                <br/>
-                <div id={"results"}></div>
-            </div>
+            <label>
+                Region
+                <select id={"region"} onClick={getRegions}></select>
+            </label>
+            <button onClick={searchRegion}>
+                Search
+            </button>
+            <br/>
+            <div id={"results"}></div>
         </Fragment>
     )
+
+    async function getRegions() {
+        try {
+            const response = await fetch("http://localhost:3000/pointsofinterest/regions");
+            const regions = await response.json()
+
+            document.getElementById("region").innerHTML = regions;
+
+            for (const region of regions) {
+                const option = document.createElement("option")
+                option.innerHTML = region.region;
+
+                document.getElementById("region").appendChild(option)
+            }
+
+        } catch(e) {
+            alert(e);
+        }
+    }
 
     async function searchRegion() {
         try {
