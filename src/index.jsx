a2b84d7dca8275f966ac8f037e756a61ff74e5a8
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-13 14:11:24 +0000

    index.jsx redefined

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom/client';
 import SearchRegion from "./components/searchRegion";
-import PointsOfInterest from "./components/pointsOfInterest";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,7 +8,6 @@ root.render(
         <div>
             Hello World from React!
             <SearchRegion/>
-            <PointsOfInterest/>
         </div>
     </Fragment>
 );
