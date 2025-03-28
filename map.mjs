91837e047802b05b664ecc2600115330ab565701
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-28 16:18:09 +0000

    map.mjs redefined

--- map.mjs
+++ map.mjs
@@ -2,11 +2,19 @@ import 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const map = L.map ("map1");
-
 const attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
 
 L.tileLayer
 ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     { attribution: attrib } ).addTo(map);
 
-map.setView([50.908,-1.4], 14);
+const pos = [50.908,-1.4]
+
+map.setView(pos, 14);
+L.marker(pos).addTo(map);
+
+map.on("click", function (event) {
+    const text = prompt("Who lives in a house like this?");
+    const marker = L.marker(event.latlng).addTo(map);
+    marker.bindPopup(`${text} (${event.latlng.lat}, ${event.latlng.lng})`);
+})
