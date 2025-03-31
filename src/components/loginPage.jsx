3fa375bd9851701feea47c1e2aaa7bae608aec14
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-31 18:42:10 +0100

    authenticationRouter.mjs defined

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,21 +1,15 @@
 import React, {Fragment} from "react";
 
 function LoginPage() {
-    if (true) {
+    const user = getLoggedIn()
+
+    if (user) {
         return (
             <Fragment>
-                <label>
-                    Username
-                    <input type={"text"} id={"username"} placeholder={"Username"} />
-                </label>
+                NAME is logged in!
                 <br/>
-                <label>
-                    Password
-                    <input type={"password"} id={"password"} placeholder={"Password"}/>
-                </label>
-                <br/>
-                <button onClick={login}>
-                    Login
+                <button onClick={logout}>
+                    Logout
                 </button>
             </Fragment>
         )
@@ -23,13 +17,33 @@ function LoginPage() {
 
     return (
         <Fragment>
-            NAME is logged in!
+            <label>
+                Username
+                <input type={"text"} id={"username"} placeholder={"Username"} />
+            </label>
             <br/>
-            <button onClick={logout}>
-                Logout
+            <label>
+                Password
+                <input type={"password"} id={"password"} placeholder={"Password"}/>
+            </label>
+            <br/>
+            <button onClick={login}>
+                Login
             </button>
         </Fragment>
     )
+
+    async function getLoggedIn() {
+        try {
+            const response = await fetch("http://localhost:3000/authentication/user");
+            const data = await response.json();
+            alert(data.username);
+            return data.username;
+
+        } catch(e) {
+            alert(e);
+        }
+    }
 
     function login() {
         alert(`${document.getElementById("username").value} ${document.getElementById("password").value}`);
