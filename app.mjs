dd0059160ae846a98f9d909937507c6b907b5809
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-31 17:50:48 +0100

    authenticationRouter.mjs defined

--- app.mjs
+++ app.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import authenticationRouter from "./routers/authenticationRouter.mjs";
 import landmarkRouter from "./routers/landmarkRouter.mjs";
 
 const app = express();
 
 app.use(express.static("public"));
+app.use("/authentication", authenticationRouter);
 app.use("/landmark", landmarkRouter)
 
 app.get("/", (req,res)=> {
---
+++ routers/authenticationRouter.mjs
@@ -0,0 +1,66 @@
+import express from 'express';
+import Database from "better-sqlite3";
+import expressSession from 'express-session';
+import betterSqlite3Session from 'express-session-better-sqlite3';
+import databaseModule from "../databaseModule.mjs";
+
+const authenticationRouter = express.Router();
+authenticationRouter.use(express.json());
+
+const sessionDatabase = new Database('session.db');
+const sqliteStore = betterSqlite3Session(expressSession, sessionDatabase);
+
+authenticationRouter.use(expressSession({
+    store: new sqliteStore(),
+    secret: 'BinnieAndClyde',
+    resave: true,
+    saveUninitialized: false,
+    rolling: true,
+    unset: 'destroy',
+    proxy: true,
+    cookie: {
+        maxAge: 600000,
+        httpOnly: false
+    }
+}));
+
+authenticationRouter.post("/login", (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+
+    if (!username) {
+        res.status(400).json({ error: "no username supplied" });
+        return;
+    }
+
+    if (!password) {
+        res.status(400).json({ error: "no password supplied" });
+        return;
+    }
+
+    try {
+        const stmt = databaseModule.prepare(
+            "SELECT * " +
+            "FROM poi_users " +
+            "WHERE username=? " +
+            "AND password=? "
+        );
+
+        const info = stmt.all(username, password);
+
+        if (info.length) {
+            req.session.username = req.body.username;
+            res.json({username: req.session.username});
+
+        } else {
+            res.status(401).json({error: "invalid login!"});
+        }
+
+    } catch(error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+export default authenticationRouter;
