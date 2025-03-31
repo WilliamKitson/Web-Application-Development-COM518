3a1b4c2c1fec13ba6a445739a4f24a87c6b0df50
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-31 16:25:51 +0100

    loginPage.jsx redefined

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,19 +1,43 @@
 import React, {Fragment} from "react";
 
 function LoginPage() {
+    if (true) {
+        return (
+            <Fragment>
+                <label>
+                    Username
+                    <input type={"text"} id={"username"} placeholder={"Username"} />
+                </label>
+                <br/>
+                <label>
+                    Password
+                    <input type={"password"} id={"password"} placeholder={"Password"}/>
+                </label>
+                <br/>
+                <button onClick={login}>
+                    Login
+                </button>
+            </Fragment>
+        )
+    }
+
     return (
         <Fragment>
-            <label>
-                Username
-                <input type={"text"} placeholder={"Username"} />
-            </label>
+            NAME is logged in!
             <br/>
-            <label>
-                Password
-                <input type={"password"} placeholder={"Password"}/>
-            </label>
+            <button onClick={logout}>
+                Logout
+            </button>
         </Fragment>
     )
+
+    function login() {
+        alert(`${document.getElementById("username").value} ${document.getElementById("password").value}`);
+    }
+
+    function logout() {
+        alert("Logged out!")
+    }
 }
 
 export default LoginPage;
