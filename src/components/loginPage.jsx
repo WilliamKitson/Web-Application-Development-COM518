92916621e93f94c66a60721de0a8b23a10662384
4kitsw10 <4kitsw10@solent.ac.uk>
2025-04-07 21:35:06 +0100

    loginPage.jsx redefined

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -26,6 +26,7 @@ function LoginPage() {
                 <button onClick={logout}>
                     Logout
                 </button>
+                <br/>
             </Fragment>
         )
     }
@@ -45,6 +46,7 @@ function LoginPage() {
             <button onClick={login}>
                 Login
             </button>
+            <br/>
         </Fragment>
     )
 
