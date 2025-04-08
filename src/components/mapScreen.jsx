fc9da2b6bc31afcdb951993dfa6810dc493f607d
4kitsw10 <4kitsw10@solent.ac.uk>
2025-04-08 16:39:01 +0100

    mapScreen.jsx redefined

--- src/components/mapScreen.jsx
+++ src/components/mapScreen.jsx
@@ -1,12 +1,40 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React, {Fragment, useEffect} from "react";
+import 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function MapScreen() {
+    useEffect(() => {
+        initialiseLeaflet()
+    })
+
     return (
         <Fragment>
-            Map Screen
+            <div id={"map1"} style={{width: "800px", height: "600px"}}>
+                Map
+            </div>
             <br/>
         </Fragment>
     )
+
+    function initialiseLeaflet() {
+        const map = L.map ("map1");
+        const attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
+
+        L.tileLayer
+        ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            { attribution: attrib } ).addTo(map);
+
+        map.setView(
+            [50.908,-1.4],
+            14
+        );
+
+        map.on("click", function (event) {
+            const text = prompt("Who lives in a house like this?");
+            const marker = L.marker(event.latlng).addTo(map);
+            marker.bindPopup(`${text} (${event.latlng.lat}, ${event.latlng.lng})`);
+        })
+    }
 }
 
 export default MapScreen;
