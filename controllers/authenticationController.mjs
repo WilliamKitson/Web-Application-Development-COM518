f58e5bfeff2222c83da93b6f55f10777ebc5f87a
4kitsw10 <4kitsw10@solent.ac.uk>
2025-04-09 16:33:56 +0100

    authenticationRouter.mjs redefined

--- controllers/authenticationController.mjs
+++ controllers/authenticationController.mjs
@@ -1,4 +1,6 @@
 import AuthenticationDao from "../dao/authenticationDao.mjs";
+import databaseModule from "../modules/databaseModule.mjs";
+import bcrypt from "bcrypt";
 
 export default class AuthenticationController {
     constructor(database) {
@@ -28,6 +30,51 @@ export default class AuthenticationController {
             );
 
             res.json(account);
+
+        } catch(error) {
+            res.status(500).json({ error: error });
+        }
+    }
+
+    async login(req, res) {
+        const {
+            username,
+            password
+        } = req.body;
+
+        if (!username) {
+            res.status(400).json({ error: "no username supplied" });
+            return;
+        }
+
+        if (!password) {
+            res.status(400).json({ error: "no password supplied" });
+            return;
+        }
+
+        try {
+            const stmt = databaseModule.prepare(
+                "SELECT * " +
+                "FROM poi_users " +
+                "WHERE username=? "
+            );
+
+            const info = stmt.all(username);
+
+            if (!info.length) {
+                res.status(401).json({error: "username not found"});
+                return;
+            }
+
+            const match = await bcrypt.compare(password, info[0].password);
+
+            if (match) {
+                req.session.username = req.body.username;
+                res.json({username: req.session.username});
+
+            } else {
+                res.status(401).json({error: "password incorrect"});
+            }
 
         } catch(error) {
             res.status(500).json({ error: error });
--- routes/authenticationRouter.mjs
+++ routes/authenticationRouter.mjs
@@ -29,51 +29,7 @@ authenticationRouter.use(expressSession({
 const authenticationController = new AuthenticationController(databaseModule);
 
 authenticationRouter.post("/register", authenticationController.register.bind(authenticationController));
-
-authenticationRouter.post("/login", async (req, res) => {
-    const {
-        username,
-        password
-    } = req.body;
-
-    if (!username) {
-        res.status(400).json({ error: "no username supplied" });
-        return;
-    }
-
-    if (!password) {
-        res.status(400).json({ error: "no password supplied" });
-        return;
-    }
-
-    try {
-        const stmt = databaseModule.prepare(
-            "SELECT * " +
-            "FROM poi_users " +
-            "WHERE username=? "
-        );
-
-        const info = stmt.all(username);
-
-        if (!info.length) {
-            res.status(401).json({error: "username not found"});
-        }
-
-        const match = await bcrypt.compare(password, info[0].password);
-
-        if (match) {
-            req.session.username = req.body.username;
-            res.json({username: req.session.username});
-
-        } else {
-            res.status(401).json({error: "password incorrect"});
-        }
-
-    } catch(error) {
-        res.status(500).json({ error: error });
-    }
-});
-
+authenticationRouter.post("/login", await authenticationController.login.bind(authenticationController));
 authenticationRouter.get('/user', authenticationController.getUser.bind(authenticationController));
 authenticationRouter.post('/logout', authenticationController.logout.bind(authenticationController));
 
